24c94dfbc059bd088c8373e69a7cc381ada59a05
RicardoAzevedo70 <117657418+RicardoAzevedo70@users.noreply.github.com>
2023-06-28 19:29:15 +0100

    commit

--- src/routes/TeamApp.js
+++ src/routes/TeamApp.js
@@ -6,11 +6,11 @@ import TeamManagement from '../pages/Dashboard/Team-Managment/TeamManagment';
  
 const TeamApp = () => {
   return (
-    <Routes>
-      <Route path="/team-players" element={<TeamPlayers/>} /> 
-      <Route path="/team-managment" element={<TeamManagement/>}/>
-      <Route path="/team-games" element={<TeamGames/>} />
-    </Routes>
+    <>
+      <Route path="team-players" element={<TeamPlayers/>} /> 
+      <Route path="team-managment" element={<TeamManagement/>}/>
+      <Route path="team-games" element={<TeamGames/>} />
+    </>
   );
 };
 
